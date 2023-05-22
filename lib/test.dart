f03edd2946c17ccb811a1a5af150fd9bc3850588
lyricMirror <nirajsahu0412@gmail.com>
2023-05-22 17:19:37 +0530

    Initial Commit

---
+++ lib/test.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+// Ideal time to initialize
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+//...
+}
+
+class Firebase {
+  static initializeApp() {
+    FirebaseAuth.instance
+        .authStateChanges()
+        .listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+  }
+
+}
